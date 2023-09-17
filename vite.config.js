a0cad0132e2eb849fedc87d1af01852dbdf3b2b2
jacob <358819135@qq.com>
2023-09-17 22:57:21 +0800

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import Markdown from 'unplugin-vue-markdown/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jacob-blog/',
+  base: '/yuxi-blog/',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/], // <-- allows Vue to compile Markdown files
